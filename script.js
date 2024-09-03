f51d0e4341f2c6c2439fa9f4c703130e92e4f272
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2024-09-03 14:38:12 -0400

    Created operate function, used to check which operation to do for evaluation

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ const multiply = function(a,b){
 const divide = function(a,b){
     return a/b;
 }
+
+const operate = function(a,b, operator){
+    if (operator === '+') add(a,b);
+    if (operator === '-') subtract(a,b);
+    if (operator === '*') multiply(a,b);
+    if (operator === '/') divide(a,b);
+}
